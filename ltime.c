c578881860deb99ab17c2c758ab40ebd9ff7247f
Daniel Loffgren <daniel@loffgren.org>
2018-06-23 16:41:35 -0700

    Both select and kqueue use a timespec timeout, so get that in there

--- ltime.c
+++ ltime.c
@@ -1,10 +1,15 @@
 
 #include <stdio.h>
 #include <sysexits.h>
+#include <sys/time.h>
 
 int main(int argc, char * const argv[]) {
 	(void)argc;
 	(void)argv;
+
+	struct timespec timeout = {
+		.tv_nsec = 1000000, /* 1ms */
+	};
 
 	printf("I'm alive\n");
 	return EX_OK;
