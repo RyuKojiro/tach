d8e8bf920c7dea3d8d4d31794023487798dfb049
Daniel Loffgren <daniel@loffgren.org>
2018-06-23 16:53:50 -0700

    Create a kevent and a kqueue

--- ltime.c
+++ ltime.c
@@ -1,15 +1,29 @@
 
+#include <err.h>
 #include <stdio.h>
-#include <sysexits.h>
+#include <sys/event.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <sysexits.h>
 
 int main(int argc, char * const argv[]) {
 	(void)argc;
 	(void)argv;
 
-	struct timespec timeout = {
+	const struct timespec timeout = {
 		.tv_nsec = 1000000, /* 1ms */
 	};
+
+	const int in = fileno(stdin);
+	const int out = fileno(stdout);
+
+	struct kevent ev;
+	EV_SET(&ev, in, EVFILT_READ, EV_ADD, 0, 0, 0);
+
+	const int kq = kqueue();
+	if (kq == -1) {
+		err(EX_OSERR, "kqueue");
+	}
 
 	printf("I'm alive\n");
 	return EX_OK;
