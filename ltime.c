2a2bf400e650b0db6488befcc29e2554bdf0df25
Daniel Loffgren <daniel@loffgren.org>
2018-06-23 17:36:29 -0700

    Added a function that subtracts two timespecs

--- ltime.c
+++ ltime.c
@@ -1,10 +1,44 @@
-
+#include <assert.h>
 #include <err.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/event.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sysexits.h>
+#include <time.h>
+
+static struct timespec timespec_subtract(const struct timespec *minuend,
+		const struct timespec *subtrahend) {
+	struct timespec result;
+
+	/*
+	 * minuend - subtrahend = result
+	 *
+	 * The minuend must be larger than the subtrahend. If they aren't, assert.
+	 */
+	assert(minuend->tv_sec >= subtrahend->tv_sec);
+	if (minuend->tv_sec == subtrahend->tv_sec) {
+		assert(minuend->tv_nsec >= subtrahend->tv_nsec);
+	}
+
+	result.tv_sec = minuend->tv_sec - subtrahend->tv_sec;
+
+	if (minuend->tv_nsec < subtrahend->tv_nsec) {
+		/*
+		 * Borrow from the seconds place.
+		 *
+		 * tv_nsec is a long, and so should be capable of holding a spare
+		 * second (in nanoseconds) on all platforms
+		 */
+		result.tv_sec--;
+		result.tv_nsec += 1000000000L;
+	}
+
+	result.tv_nsec = minuend->tv_nsec - subtrahend->tv_nsec;
+
+	return result;
+}
 
 int main(int argc, char * const argv[]) {
 	(void)argc;
