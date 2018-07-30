24c5762369e8a95803aeda23eb24678f91c91c46
Daniel Loffgren <daniel@loffgren.org>
2018-07-29 21:46:32 -0700

    Added a test that prints one char at a time, ending each line with an "nl\n"

---
+++ tests/one_char_at_a_time.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+
+from time import sleep
+from math import sin
+import sys
+
+x = 0
+while True:
+    x = x + 1
+    y = 10.0 * sin(x / 4.0)
+    for i in range(0, int(abs(y))):
+        sys.stdout.write("+")
+        sys.stdout.flush()
+        sleep(.3)
+    print "nl"
