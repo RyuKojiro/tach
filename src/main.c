c9e99d6ee7b88fd1f5e0ba2016beb46175d61773
Daniel Loffgren <daniel@loffgren.org>
2019-07-08 22:30:52 -0700

    Prefixing the timestamp region with COLOR_RESET should be done on either all prints, or no prints

--- src/main.c
+++ src/main.c
@@ -229,7 +229,7 @@ int main(int argc, char * const argv[]) {
 			printf(TS_FMT "%s%s\r", TS_ARG(diff), sep, lb->buf);
 		} else if (!first && !slow) {
 			/* Normal idle timestamp update */
-			printf(COLOR_RESET TS_FMT "%s\r", TS_ARG(diff), sep);
+			printf(TS_FMT "%s\r", TS_ARG(diff), sep);
 		}
 		fflush(stdout);
 
