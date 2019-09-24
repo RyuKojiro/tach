c5500f7702bfff3a545ae2f1ec90e652284fd306
Daniel Loffgren <daniel@loffgren.org>
2019-09-23 23:43:21 -0700

    Add a test script that creates a known sequence of fd oscillations, for later comparison

---
+++ tests/shared_sequence.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3.7
+
+import sys
+
+seq = [3,2,1,1,2,3,4]
+names = ["stdout", "stderr" ]
+fds = [sys.stdout, sys.stderr]
+
+for i in range(len(seq)):
+    fd = i % 2
+    print((names[fd] + "\n") * seq[i], file=fds[fd], end="")
