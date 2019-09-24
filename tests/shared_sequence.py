5fb6a12ef636ed62b0264fe9b7745123cb060776
Daniel Loffgren <daniel@loffgren.org>
2019-09-24 00:15:30 -0700

    Enable test 3b, with a sleep to make sure there is a correct muxing order

--- tests/shared_sequence.py
+++ tests/shared_sequence.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.7
 
+from time import sleep
 import sys
 
 seq = [3,2,1,1,2,3,4]
@@ -10,3 +11,4 @@ for i in range(len(seq)):
     fd = i % 2
     print((names[fd] + "\n") * seq[i], file=fds[fd], end="")
     fds[fd].flush()
+    sleep(.01)
